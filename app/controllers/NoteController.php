39950e1b1a0d4885f3bbeaf3738870931c55b7fd
h2jie <h2jie09@gmail.com>
2018-04-18 20:20:56 +0200

    Middleware "configurado"

--- app/controllers/NoteController.php
+++ app/controllers/NoteController.php
@@ -4,12 +4,15 @@ use Slim\Http\Response;
 
 class NoteController
 {
-    public function firstTest(Request $request, Response $response){
+    public function firstTest(Request $request, Response $response, $args = []){
 
         $arrayToReturn = array('name' => 'Jose', 'edad'=> 40);
         $newResponse = $response->withJson($arrayToReturn,200);
-        return $newResponse;
+        $response->getBody()->write( $newResponse);
+        return $response;
     }
+
+
 }
 
 
